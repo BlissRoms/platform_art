hopez13/art

ART: Introduce stackmap with no DexRegInfo.
Some of safepoints don't need to have DexRegisterMap info;
this will decrease the stackmap size.

.oat file size reduction:
 - boot.oat:           -233 kb (-5.4%)
 - boot-framework.oat: -704 kb (-4.9%)

Test: 461-get-reference-vreg, 466-get-live-vreg.
Test: 543-env-long-ref, 616-cha*.
Test: test-art-target, +gc-stress.

Change-Id: Idbad355770e30a30dcf14127642e03ee666878b8

runtime/jit/jit.cc
@@ -470,6 +470,7 @@ bool Jit::MaybeDoOnStackReplacement(Thread* thread,
     // We found a stack map, now fill the frame with dex register values from the interpreter's
     // shadow frame.
     DexRegisterMap vreg_map = code_info.GetDexRegisterMapOf(stack_map);
+    CHECK_EQ(vreg_map.size(), number_of_vregs);
 
     frame_size = osr_method->GetFrameSizeInBytes();
 
@@ -489,7 +490,6 @@ bool Jit::MaybeDoOnStackReplacement(Thread* thread,
       // If we don't have a dex register map, then there are no live dex registers at
       // this dex pc.
     } else {
-      DCHECK_EQ(vreg_map.size(), number_of_vregs);
       for (uint16_t vreg = 0; vreg < number_of_vregs; ++vreg) {
         DexRegisterLocation::Kind location = vreg_map[vreg].GetKind();
         if (location == DexRegisterLocation::Kind::kNone) {

BlissRoms/platform_art

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

runtime/jit/jit.cc
@@ -472,6 +472,7 @@ bool Jit::MaybeDoOnStackReplacement(Thread* thread,
     // We found a stack map, now fill the frame with dex register values from the interpreter's
     // shadow frame.
     DexRegisterMap vreg_map = code_info.GetDexRegisterMapOf(stack_map);
+    DCHECK_EQ(vreg_map.size(), number_of_vregs);
 
     frame_size = osr_method->GetFrameSizeInBytes();
 
@@ -491,7 +492,6 @@ bool Jit::MaybeDoOnStackReplacement(Thread* thread,
       // If we don't have a dex register map, then there are no live dex registers at
       // this dex pc.
     } else {
-      DCHECK_EQ(vreg_map.size(), number_of_vregs);
       for (uint16_t vreg = 0; vreg < number_of_vregs; ++vreg) {
         DexRegisterLocation::Kind location = vreg_map[vreg].GetKind();
         if (location == DexRegisterLocation::Kind::kNone) {